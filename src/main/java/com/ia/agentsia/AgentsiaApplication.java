45a9bd26b64d4050fefcd979db8661126c81f2c7
dev-miguel-resources <ingenieromiguelch@gmail.com>
2026-08-24 22:15:19 -0400

    first commit

---
+++ src/main/java/com/ia/agentsia/AgentsiaApplication.java
@@ -0,0 +1,13 @@
+package com.ia.agentsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentsiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ia/agentsia/AgentsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.agentsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentsiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
